9139f135d7408885a334c652f3440816cf1731e4
joyfuluj <joyumejiego@yahoo.com>
2025-01-21 18:14:22 -0800

    Added Jump movement

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,10 +4,27 @@ public class BallController : MonoBehaviour
 {
     public Rigidbody sphereRigidbody;
     public float ballSpeed = 2f;
+    public float jumpHeight = 4f;
+    public bool onGround = false;
     
-    public void MoveBall(Vector2 input)
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Floor")
+        {
+            onGround = true;
+        }
+    }
+    
+    public void MoveBall(Vector3 input)
     {
         Vector3 inputXZPlane = new(input.x, 0, input.y);
+
+        if(input.z > 0 && onGround)
+        {
+            sphereRigidbody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            onGround = false;
+        }
+
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
     }
 
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,30 +3,29 @@ using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
-    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>();
 
-    // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = Vector2.zero;
+        Vector3 inputVector = Vector3.zero;
         if(Input.GetKey(KeyCode.W)){
-            // Debug.Log(Vector2.up);
-            inputVector += Vector2.up;
+            inputVector += Vector3.up;
         }
 
         if(Input.GetKey(KeyCode.S)){
-            // Debug.Log(Vector2.down);
-            inputVector += Vector2.down;
+            inputVector += Vector3.down;
         }
 
         if(Input.GetKey(KeyCode.D)){
-            // Debug.Log(Vector2.right);
-            inputVector += Vector2.right;
+            inputVector += Vector3.right;
         }
 
         if(Input.GetKey(KeyCode.A)){
-            // Debug.Log(Vector2.left);
-            inputVector += Vector2.left;
+            inputVector += Vector3.left;
+        }
+
+        if(Input.GetKey(KeyCode.Space)){
+            inputVector += Vector3.forward;
         }
 
         OnMove?.Invoke(inputVector);
